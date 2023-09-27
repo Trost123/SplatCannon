e761d0333f93badb16fe74f4f4e80290d4ae37c7
Trost <ekolodya@gmail.com>
2023-09-27 17:11:26 +0300

    Camera recoil by rotation.

--- Assets/Scripts/RecoilAnimation.cs
+++ Assets/Scripts/RecoilAnimation.cs
@@ -2,25 +2,26 @@ using UnityEngine;
 
 public class RecoilAnimation : MonoBehaviour
 {
-    public Transform cameraTransform;  // Reference to the camera's transform
+    public Transform objectTransform;  // Reference to the object's transform
     public float recoilDuration = 0.1f;  // Duration of the recoil
     public float recoilStrength = 0.1f;  // Strength of the recoil
 
+    protected float recoilTimer;  // Timer for the recoil duration
+
     private Vector3 originalPosition;  // Store the original position of the camera
-    private Vector3 recoilOffset;  
-    private float recoilTimer;  // Timer for the recoil duration
+    private Vector3 recoilOffset;
 
     private void Start()
     {
         // Store the original position of the camera
-        originalPosition = cameraTransform.localPosition;
+        originalPosition = objectTransform.localPosition;
         recoilOffset = new Vector3(0, 0, recoilStrength);
     }
 
-    public void ApplyRecoil()
+    public virtual void ApplyRecoil()
     {
         // Apply recoil by moving the camera's position
-        cameraTransform.localPosition += recoilOffset;
+        objectTransform.localPosition += recoilOffset;
 
         // Start the recoil timer
         recoilTimer = recoilDuration;
@@ -37,13 +38,13 @@ public class RecoilAnimation : MonoBehaviour
             float progress = 1 - Mathf.Clamp01(recoilTimer / recoilDuration);
 
             // Lerp between the recoiled position and the original position based on progress and recoilTimer
-            cameraTransform.localPosition = Vector3.Lerp(originalPosition + recoilOffset, originalPosition, progress);
+            objectTransform.localPosition = Vector3.Lerp(originalPosition + recoilOffset, originalPosition, progress);
 
             // Ensure that the recoil timer doesn't go below zero
             if (recoilTimer <= 0)
             {
                 // Snap the position to the original position when the recoil is complete
-                cameraTransform.localPosition = originalPosition;
+                objectTransform.localPosition = originalPosition;
             }
         }
     }
---
+++ Assets/Scripts/RotationRecoil.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class RotationRecoil : RecoilAnimation
+{
+    private Quaternion originalRotation; // Store the original rotation of the object
+
+    private void Start()
+    {
+        // Store the original rotation of the object
+        originalRotation = objectTransform.localRotation;
+    }
+
+    public override void ApplyRecoil()
+    {
+        // Apply recoil by slightly rotating the object
+        Vector3 randomTiltDirection = new Vector3(Random.Range(-5f, 5f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        Quaternion recoilRotation = Quaternion.Euler(randomTiltDirection * recoilStrength);
+        objectTransform.localRotation *= recoilRotation;
+
+        // Start the recoil timer
+        recoilTimer = recoilDuration;
+    }
+
+    public void Update()
+    {
+        // If the recoil timer is active, reduce the timer
+        if (recoilTimer > 0)
+        {
+            recoilTimer -= Time.deltaTime;
+
+            // Calculate the progress of the recoil animation (a value between 0 and 1)
+            float progress = 1 - Mathf.Clamp01(recoilTimer / recoilDuration);
+
+            // Lerp between the current rotation and the original rotation
+            objectTransform.localRotation = Quaternion.Slerp(objectTransform.localRotation, originalRotation, progress);
+
+            // Ensure that the recoil timer doesn't go below zero
+            if (recoilTimer <= 0)
+            {
+                // Snap the rotation to the original rotation when the recoil is complete
+                objectTransform.localRotation = originalRotation;
+            }
+        }
+    }
+}
