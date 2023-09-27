228477b4ef9c3516afdbb6040facb04995325f37
Trost <ekolodya@gmail.com>
2023-09-27 15:21:52 +0300

    Projectile explosion FX

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,32 +7,40 @@ internal class Projectile : MonoBehaviour
     
     private int _collisionCount;
     private int _maxCollisionCount = 3;
+
+    private bool _isExploded;
+
+    [SerializeField] private ParticleSystem[] _particleSystems;
     
 
     private void Start()
     {
-         _collisionDistance = transform.localScale.x * 0.5f;
+         _collisionDistance = transform.localScale.x;
     }
 
     public void SetShotParameters(Vector3 shootPointForward, float shootingForce)
     {
-        _currentVelocity = shootPointForward * 0.1f * shootingForce;
+        _currentVelocity = shootPointForward * 0.4f * shootingForce;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
+        if (_isExploded) return;
         // Calculate the next position and velocity using physics (including gravity)
         transform.position += _currentVelocity * Time.fixedDeltaTime;
-        _currentVelocity += Physics.gravity * 0.05f * Time.fixedDeltaTime;
+        _currentVelocity += Physics.gravity * 0.8f * Time.fixedDeltaTime;
 
         CollisionCheckRaycast();
     }
-    
+
     private void CollisionCheckRaycast()
     {
         // Get the direction of the projectile's velocity
         Vector3 rayDirection = _currentVelocity;
-
+        
+        // Debug visualization of the ray
+        Debug.DrawRay(transform.position, rayDirection.normalized * _collisionDistance, Color.red);
+        
         // Cast a ray in the forward direction of the projectile
         RaycastHit hit;
         if (Physics.Raycast(transform.position, rayDirection, out hit, _collisionDistance))
@@ -43,7 +51,7 @@ internal class Projectile : MonoBehaviour
                 _currentVelocity = Vector3.Reflect(_currentVelocity, hit.normal);
                 
                 // Apply velocity damping (adjust the value as needed)
-                float dampingFactor = 0.8f; // You can adjust this value
+                float dampingFactor = 0.6f; // You can adjust this value
                 _currentVelocity *= dampingFactor;
                 
                 DecalSystem decalSystem = hit.collider.gameObject.GetComponent<DecalSystem>();
@@ -62,7 +70,17 @@ internal class Projectile : MonoBehaviour
 
                 if (_collisionCount == _maxCollisionCount)
                 {
-                    Destroy(gameObject);
+                    _isExploded = true;
+                    // Disable the mesh renderer
+                    GetComponent<MeshRenderer>().enabled = false;
+                    
+                    // Play all particle systems
+                    foreach (var ps in _particleSystems)
+                    {
+                            ps.Play();
+                    }
+                    
+                    Destroy(gameObject, 1f);
                 }
             }
         }
